4f4afb62195e31a78c55cfca8d8b126d36fa9786
santia87 <ssr18018@gmail.com>
2017-02-28 00:22:24 -0500

    added to the homepage and created custom archives page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,6 +25,35 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
+<!--starts homwe page images-->
+
+<section class="featured-work">
+	<div class="site-content">
+
+	<h4>Featured Work</h4>
+
+	<ul class="homepage-featured-work">
+	<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				<?php while ( have_posts() ) : the_post(); 
+					$image_1 = get_field("image_1");
+					$size = "medium";
+				?>
+				</li class="individual-featured-work">
+					<figure>
+						<?php echo wp_get_attachment_image($image_1, $size); ?>
+					</figure>
+				</li>
+
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+				<?php endwhile; // end of loop ?>
+	<?php wp_reset_query(); // resets the altered query back to the original ?>
+	</ul>			
+	</div>
+</section>
+
+<!--Starts home page blog-->
+
 <section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
@@ -35,8 +64,8 @@ get_header(); ?>
 					<?php the_excerpt(); ?>
 					<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
 
-				<?php endwhile; ?>
-			<?php wp_reset_query(); ?>
+				<?php endwhile; // end of loop ?>
+			<?php wp_reset_query();  // resets the altered query back to the original ?>
 
 		</div>
 	</div>
