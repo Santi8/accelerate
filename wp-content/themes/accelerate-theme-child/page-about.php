50b8e0a5e729d0163472f4996f7a3eb92a8a6880
santia87 <ssr18018@gmail.com>
2017-03-17 15:59:35 -0400

    re ordered the about page, restructrued the query, defined the description_1 variable

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -10,10 +10,21 @@
 get_header(); ?>
 
 <section class="home-page">
-	<div class="site-content">
-	<?php query_posts('posts_per_page=3&post_type=services'); ?> <!--added this code to see if it would help, hasn't-->
-		<?php while ( have_posts() ) : the_post(); 
+	<div class='homepage-hero'>
+				<article class="about-info"><span>Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</article>
+			</div>
+</section><!-- .home-page -->
 
+<div class="site-content">
+		<h2 class="services">Our Services</h2>
+		<p class="about-info">We take pride in our clients and the content we create for them. <br> Here's a brief overview of our offered services. </p>
+
+<section class="about">
+	<?php query_posts('post_type=services'); ?> <!--added this code to see if it would help, hasn't-->
+	
+		Are you working yet???
+
+		<?php while ( have_posts() ) : the_post(); 
 			$description_1 = get_field('description_1');
 			$description_2 = get_field('description_2');
 			$description_3 = get_field('description_3');
@@ -25,21 +36,12 @@ get_header(); ?>
 			$size = "full";
 		?>
 
-		<div class='homepage-hero'>
-				<article class="about-info"><span>Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</article>
-			</div>
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
-</section><!-- .home-page -->
-			
-<section class="about">
-
-	<h2 class="services">Our Services</h2>
-	<p class="about-info">We take pride in our clients and the content we create for them. <br> Here's a brief overview of our offered services. </p>
-			<article class="description">
+		<article class="description">
 				<aside class="service-description">
 					<!--<h2><?php //the_title(); ?></h2>-->
-					<p class="info-about"><?php echo $description; ?></p>
+				<p class="info-about"><?php echo $description_1; ?></p>
+
+
 						<?php the_content(); ?>
 
 				</aside>
@@ -47,9 +49,14 @@ get_header(); ?>
 					<?php if($image_1) { ?>
 						<?php echo wp_get_attachment_image( $image_1, $size ); ?>
 					<?php } ?>
-				</div>
-			</article>
-</section><!--end of about section-->
+				</div><!--close single image div-->
+			</article><!--closes description-->
+
+			<?php endwhile; // end of the loop.
+			wp_reset_query();  ?>
+		</section><!--end of about section-->
+
+	</div><!-- .container -->
 
 
 <?php get_footer(); ?>
